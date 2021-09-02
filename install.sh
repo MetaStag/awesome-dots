43de3d1b4e19b4231a3a0f81559f71b4483254bc
Yumi <82706323+Yumidev00@users.noreply.github.com>
2021-09-02 19:31:47 +0200

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,22 @@
+######################################
+############>INSTALLATION<##############
+######################################
+#!/bin/bash
+
+cd $HOME/
+git clone https://github.com/MetaStag/awesome-dots.git
+cd awesome-dots
+cp -r alacritty $HOME/.config/
+cp -r nvim $HOME/.config/
+cp -r polybar $HOME/.config/
+cp -r rofi $HOME/.config/
+cp -r termite $HOME/.config/
+cp -r .zshrc $HOME/.config/
+cp -r awesome $HOME/.config/
+cp -r dunst $HOME/.config/
+cp -r glava $HOME/.config/
+cp -r .moc $HOME/.config/
+cp -r picom.conf $HOME/.config/
+cp -r sddm $HOME/.config/
+cp -r zathura $HOME/.config/
+echo "Logout or Restart WM :)"
