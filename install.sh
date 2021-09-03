3b6a1fdce559037f4d88debfeb8d45656620e391
Yumi <82706323+Yumidev00@users.noreply.github.com>
2021-09-03 23:25:58 +0200

    Update install.sh
    I hope it works fine. :')

--- install.sh
+++ install.sh
@@ -1,23 +1,23 @@
-######################################
-############>INSTALLATION<##############
-######################################
 #!/bin/bash
 
-cd $HOME/
-git clone https://github.com/MetaStag/awesome-dots.git
-cd awesome-dots
+######################################
+###########>INSTALLATION<#############
+######################################
+
 cp -r alacritty $HOME/.config/
 cp -r nvim $HOME/.config/
 cp -r polybar $HOME/.config/
 cp -r rofi $HOME/.config/
 cp -r termite $HOME/.config/
-cp -r .zshrc $HOME/.config/
+cp -r .zshrc $HOME/
 cp -r awesome $HOME/.config/
 cp -r dunst $HOME/.config/
 cp -r glava $HOME/.config/
-cp -r .moc $HOME/.config/
+cp -r .moc $HOME/
 cp -r picom.conf $HOME/.config/
 cp -r sddm $HOME/.config/
 cp -r zathura $HOME/.config/
+cd ../
 rm -rf $HOME/awesome-dots
+
 echo "Logout or Restart WM :)"
